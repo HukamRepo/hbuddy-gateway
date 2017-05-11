8afaeeba3780cc0fde071cff8e80b81ea10dae7d
Gurvinder Singh <gurvsin3@in.ibm.com>
2017-05-11 20:26:02 +0530

    small change 2

--- app/handlers/ble/blenoHandler.js
+++ app/handlers/ble/blenoHandler.js
@@ -1,241 +1,235 @@
 
-var HBuddyService = require('../../handlers/ble/hbuddyService');
-var primaryService = new HBuddyService();
+//var HBuddyService = require('../../handlers/ble/hbuddyService');
+//var primaryService = new HBuddyService();
 
 exports.advertise = function(gatewayInfo) {
 	
-	var uuid = "C7841029FE7C48948532F97908EF1AE4";
-	
-	if(gatewayInfo && gatewayInfo.gatewayId){
-//		uuid = gatewayInfo.gatewayId; 
-	}	 
-    
-	  var util = require('util');
-	  var bleno = require('bleno');
+	var util = require('util');
 
-	  var BlenoPrimaryService = bleno.PrimaryService;
-	  var BlenoCharacteristic = bleno.Characteristic;
-	  var BlenoDescriptor = bleno.Descriptor;
+	var bleno = require('bleno');
 
-	  console.log('bleno for gatewayInfo: ', gatewayInfo);
 
-	  /*
-	  var StaticReadOnlyCharacteristic = function() {
-	    StaticReadOnlyCharacteristic.super_.call(this, {
-	      uuid: uuid,
-	      properties: ['read'],
-	      value: new Buffer('value'),
-	      descriptors: [
-	        new BlenoDescriptor({
-	          uuid: uuid,
-	          value: 'hBuddy Gateway'
-	        })
-	      ]
-	    });
-	  };
-	  util.inherits(StaticReadOnlyCharacteristic, BlenoCharacteristic);
+	var BlenoPrimaryService = bleno.PrimaryService;
+	var BlenoCharacteristic = bleno.Characteristic;
+	var BlenoDescriptor = bleno.Descriptor;
 
-	  var DynamicReadOnlyCharacteristic = function() {
-	    DynamicReadOnlyCharacteristic.super_.call(this, {
-	      uuid: "9A0F8BE0F89C48FEB11706A2EC7C3792",
-	      properties: ['read']
-	    });
-	  };
+	console.log('bleno');
 
-	  util.inherits(DynamicReadOnlyCharacteristic, BlenoCharacteristic);
+	var StaticReadOnlyCharacteristic = function() {
+	  StaticReadOnlyCharacteristic.super_.call(this, {
+	    uuid: 'fffffffffffffffffffffffffffffff1',
+	    properties: ['read'],
+	    value: new Buffer('value'),
+	    descriptors: [
+	      new BlenoDescriptor({
+	        uuid: '2901',
+	        value: 'user description'
+	      })
+	    ]
+	  });
+	};
+	util.inherits(StaticReadOnlyCharacteristic, BlenoCharacteristic);
 
-	  DynamicReadOnlyCharacteristic.prototype.onReadRequest = function(offset, callback) {
-	    var result = this.RESULT_SUCCESS;
-	    var data = new Buffer('dynamic value');
+	var DynamicReadOnlyCharacteristic = function() {
+	  DynamicReadOnlyCharacteristic.super_.call(this, {
+	    uuid: 'fffffffffffffffffffffffffffffff2',
+	    properties: ['read']
+	  });
+	};
 
-	    if (offset > data.length) {
-	      result = this.RESULT_INVALID_OFFSET;
-	      data = null;
-	    } else {
-	      data = data.slice(offset);
-	    }
+	util.inherits(DynamicReadOnlyCharacteristic, BlenoCharacteristic);
 
-	    callback(result, data);
-	  };
+	DynamicReadOnlyCharacteristic.prototype.onReadRequest = function(offset, callback) {
+	  var result = this.RESULT_SUCCESS;
+	  var data = new Buffer('dynamic value');
 
-	  
-	  var LongDynamicReadOnlyCharacteristic = function() {
-	    LongDynamicReadOnlyCharacteristic.super_.call(this, {
-	      uuid: uuid,
-	      properties: ['read']
-	    });
-	  };
-
-	  util.inherits(LongDynamicReadOnlyCharacteristic, BlenoCharacteristic);
-
-	  LongDynamicReadOnlyCharacteristic.prototype.onReadRequest = function(offset, callback) {
-	    var result = this.RESULT_SUCCESS;
-	    var data = new Buffer(512);
-
-	    for (var i = 0; i < data.length; i++) {
-	      data[i] = i % 256;
-	    }
-
-	    if (offset > data.length) {
-	      result = this.RESULT_INVALID_OFFSET;
-	      data = null;
-	    } else {
-	      data = data.slice(offset);
-	    }
-
-	    callback(result, data);
-	  };
-	  
-	  var NotifyOnlyCharacteristic = function() {
-	    NotifyOnlyCharacteristic.super_.call(this, {
-	      uuid: uuid,
-	      properties: ['notify']
-	    });
-	  };
-
-	  util.inherits(NotifyOnlyCharacteristic, BlenoCharacteristic);
-
-	  NotifyOnlyCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
-	    console.log('NotifyOnlyCharacteristic subscribe');
-
-	    this.counter = 0;
-	    this.changeInterval = setInterval(function() {
-	      var data = new Buffer(4);
-	      data.writeUInt32LE(this.counter, 0);
-
-	      console.log('NotifyOnlyCharacteristic update value: ' + this.counter);
-	      updateValueCallback(data);
-	      this.counter++;
-	    }.bind(this), 5000);
-	  };
-
-	  NotifyOnlyCharacteristic.prototype.onUnsubscribe = function() {
-	    console.log('NotifyOnlyCharacteristic unsubscribe');
-
-	    if (this.changeInterval) {
-	      clearInterval(this.changeInterval);
-	      this.changeInterval = null;
-	    }
-	  };
-
-	  NotifyOnlyCharacteristic.prototype.onNotify = function() {
-	    console.log('NotifyOnlyCharacteristic on notify');
-	  };
-
-	  var IndicateOnlyCharacteristic = function() {
-	    IndicateOnlyCharacteristic.super_.call(this, {
-	      uuid: uuid,
-	      properties: ['indicate']
-	    });
-	  };
-
-	  util.inherits(IndicateOnlyCharacteristic, BlenoCharacteristic);
-
-	  IndicateOnlyCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
-	    console.log('IndicateOnlyCharacteristic subscribe');
-
-	    this.counter = 0;
-	    this.changeInterval = setInterval(function() {
-	      var data = new Buffer(4);
-	      data.writeUInt32LE(this.counter, 0);
-
-	      console.log('IndicateOnlyCharacteristic update value: ' + this.counter);
-	      updateValueCallback(data);
-	      this.counter++;
-	    }.bind(this), 1000);
-	  };
-
-	  IndicateOnlyCharacteristic.prototype.onUnsubscribe = function() {
-	    console.log('IndicateOnlyCharacteristic unsubscribe');
-
-	    if (this.changeInterval) {
-	      clearInterval(this.changeInterval);
-	      this.changeInterval = null;
-	    }
-	  };
-
-	  IndicateOnlyCharacteristic.prototype.onIndicate = function() {
-	    console.log('IndicateOnlyCharacteristic on indicate');
-	  };
-	  
-	  */
-
-	  var WriteOnlyCharacteristic = function() {
-	    WriteOnlyCharacteristic.super_.call(this, {
-	      uuid: "9A0F8BE0F89C48FEB11706A2EC7C3792",
-	      properties: ['write', 'writeWithoutResponse']
-	    });
-	  };
-
-	  util.inherits(WriteOnlyCharacteristic, BlenoCharacteristic);
-
-	  WriteOnlyCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
-	    console.log('WriteOnlyCharacteristic write request: ' + data.toString('hex') + ' ' + offset + ' ' + withoutResponse);
-
-	    callback(this.RESULT_SUCCESS);
-	  };
-
-	  function SampleService() {
-	    SampleService.super_.call(this, {
-	      uuid: uuid,
-	      characteristics: [
-//	        new StaticReadOnlyCharacteristic(),
-//	        new DynamicReadOnlyCharacteristic(),
-//	        new LongDynamicReadOnlyCharacteristic(),
-	        new WriteOnlyCharacteristic(),
-//	        new NotifyOnlyCharacteristic(),
-//	        new IndicateOnlyCharacteristic()
-	      ]
-	    });
+	  if (offset > data.length) {
+	    result = this.RESULT_INVALID_OFFSET;
+	    data = null;
+	  } else {
+	    data = data.slice(offset);
 	  }
 
-	  util.inherits(SampleService, BlenoPrimaryService);
+	  callback(result, data);
+	};
 
-	  bleno.on('stateChange', function(state) {
-	    console.log('on -> stateChange: ' + state + ', address = ' + bleno.address);
-
-	    if (state === 'poweredOn') {
-	      bleno.startAdvertising('hBuddy-gateway', [uuid]);
-	    } else {
-	      bleno.stopAdvertising();
-	    }
+	var LongDynamicReadOnlyCharacteristic = function() {
+	  LongDynamicReadOnlyCharacteristic.super_.call(this, {
+	    uuid: 'fffffffffffffffffffffffffffffff3',
+	    properties: ['read']
 	  });
+	};
 
-	  // Linux only events /////////////////
-	  bleno.on('accept', function(clientAddress) {
-	    console.log('on -> accept, client: ' + clientAddress);
-	    bleno.updateRssi();
-	  });
+	util.inherits(LongDynamicReadOnlyCharacteristic, BlenoCharacteristic);
 
-	  bleno.on('disconnect', function(clientAddress) {
-	    console.log('on -> disconnect, client: ' + clientAddress);
-	  });
+	LongDynamicReadOnlyCharacteristic.prototype.onReadRequest = function(offset, callback) {
+	  var result = this.RESULT_SUCCESS;
+	  var data = new Buffer(512);
 
-	  bleno.on('rssiUpdate', function(rssi) {
-	    console.log('on -> rssiUpdate: ' + rssi);
-	  });
-	  //////////////////////////////////////
+	  for (var i = 0; i < data.length; i++) {
+	    data[i] = i % 256;
+	  }
 
-	  bleno.on('mtuChange', function(mtu) {
-	    console.log('on -> mtuChange: ' + mtu);
-	  });
+	  if (offset > data.length) {
+	    result = this.RESULT_INVALID_OFFSET;
+	    data = null;
+	  } else {
+	    data = data.slice(offset);
+	  }
 
-	  bleno.on('advertisingStart', function(error) {
-	    console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
-	    if (!error) {
-	      bleno.setServices([
-	        new SampleService()
-	      ]);
-	    }
-	  });
+	  callback(result, data);
+	};
 
-	  bleno.on('advertisingStop', function() {
-	    console.log('on -> advertisingStop');
+	var WriteOnlyCharacteristic = function() {
+	  WriteOnlyCharacteristic.super_.call(this, {
+	    uuid: 'fffffffffffffffffffffffffffffff4',
+	    properties: ['write', 'writeWithoutResponse']
 	  });
+	};
 
-	  bleno.on('servicesSet', function(error) {
-	    console.log('on -> servicesSet: ' + (error ? 'error ' + error : 'success'));
+	util.inherits(WriteOnlyCharacteristic, BlenoCharacteristic);
+
+	WriteOnlyCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
+	  console.log('WriteOnlyCharacteristic write request: ' + data.toString('hex') + ' ' + offset + ' ' + withoutResponse);
+
+	  callback(this.RESULT_SUCCESS);
+	};
+
+	var NotifyOnlyCharacteristic = function() {
+	  NotifyOnlyCharacteristic.super_.call(this, {
+	    uuid: 'fffffffffffffffffffffffffffffff5',
+	    properties: ['notify']
 	  });
-	  
-    
+	};
+
+	util.inherits(NotifyOnlyCharacteristic, BlenoCharacteristic);
+
+	NotifyOnlyCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
+	  console.log('NotifyOnlyCharacteristic subscribe');
+
+	  this.counter = 0;
+	  this.changeInterval = setInterval(function() {
+	    var data = new Buffer(4);
+	    data.writeUInt32LE(this.counter, 0);
+
+	    console.log('NotifyOnlyCharacteristic update value: ' + this.counter);
+	    updateValueCallback(data);
+	    this.counter++;
+	  }.bind(this), 5000);
+	};
+
+	NotifyOnlyCharacteristic.prototype.onUnsubscribe = function() {
+	  console.log('NotifyOnlyCharacteristic unsubscribe');
+
+	  if (this.changeInterval) {
+	    clearInterval(this.changeInterval);
+	    this.changeInterval = null;
+	  }
+	};
+
+	NotifyOnlyCharacteristic.prototype.onNotify = function() {
+	  console.log('NotifyOnlyCharacteristic on notify');
+	};
+
+	var IndicateOnlyCharacteristic = function() {
+	  IndicateOnlyCharacteristic.super_.call(this, {
+	    uuid: 'fffffffffffffffffffffffffffffff6',
+	    properties: ['indicate']
+	  });
+	};
+
+	util.inherits(IndicateOnlyCharacteristic, BlenoCharacteristic);
+
+	IndicateOnlyCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
+	  console.log('IndicateOnlyCharacteristic subscribe');
+
+	  this.counter = 0;
+	  this.changeInterval = setInterval(function() {
+	    var data = new Buffer(4);
+	    data.writeUInt32LE(this.counter, 0);
+
+	    console.log('IndicateOnlyCharacteristic update value: ' + this.counter);
+	    updateValueCallback(data);
+	    this.counter++;
+	  }.bind(this), 1000);
+	};
+
+	IndicateOnlyCharacteristic.prototype.onUnsubscribe = function() {
+	  console.log('IndicateOnlyCharacteristic unsubscribe');
+
+	  if (this.changeInterval) {
+	    clearInterval(this.changeInterval);
+	    this.changeInterval = null;
+	  }
+	};
+
+	IndicateOnlyCharacteristic.prototype.onIndicate = function() {
+	  console.log('IndicateOnlyCharacteristic on indicate');
+	};
+
+	function SampleService() {
+	  SampleService.super_.call(this, {
+	    uuid: 'fffffffffffffffffffffffffffffff0',
+	    characteristics: [
+	      new StaticReadOnlyCharacteristic(),
+	      new DynamicReadOnlyCharacteristic(),
+	      new LongDynamicReadOnlyCharacteristic(),
+	      new WriteOnlyCharacteristic(),
+	      new NotifyOnlyCharacteristic(),
+	      new IndicateOnlyCharacteristic()
+	    ]
+	  });
+	}
+
+	util.inherits(SampleService, BlenoPrimaryService);
+
+	bleno.on('stateChange', function(state) {
+	  console.log('on -> stateChange: ' + state + ', address = ' + bleno.address);
+
+	  if (state === 'poweredOn') {
+	    bleno.startAdvertising('test', ['fffffffffffffffffffffffffffffff0']);
+	  } else {
+	    bleno.stopAdvertising();
+	  }
+	});
+
+	// Linux only events /////////////////
+	bleno.on('accept', function(clientAddress) {
+	  console.log('on -> accept, client: ' + clientAddress);
+
+	  bleno.updateRssi();
+	});
+
+	bleno.on('disconnect', function(clientAddress) {
+	  console.log('on -> disconnect, client: ' + clientAddress);
+	});
+
+	bleno.on('rssiUpdate', function(rssi) {
+	  console.log('on -> rssiUpdate: ' + rssi);
+	});
+	//////////////////////////////////////
+
+	bleno.on('mtuChange', function(mtu) {
+	  console.log('on -> mtuChange: ' + mtu);
+	});
+
+	bleno.on('advertisingStart', function(error) {
+	  console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+
+	  if (!error) {
+	    bleno.setServices([
+	      new SampleService()
+	    ]);
+	  }
+	});
+
+	bleno.on('advertisingStop', function() {
+	  console.log('on -> advertisingStop');
+	});
+
+	bleno.on('servicesSet', function(error) {
+	  console.log('on -> servicesSet: ' + (error ? 'error ' + error : 'success'));
+	});
+
+	
 }
