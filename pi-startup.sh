b16261d7d3efcbe3e036d15ef154ae6ae40528cc
Gurvinder Singh <gurvsin3@in.ibm.com>
2017-08-16 17:58:12 +0530

    Minor changes

--- pi-startup.sh
+++ pi-startup.sh
@@ -7,7 +7,10 @@ sudo chmod 755 -R /tmp/motion
 
 sudo modprobe bcm2835-v4l2
 
-docker run --rm -d -p 9000:9000 --name gateway-app -v /tmp:/tmp --privileged -it hukam/gateway-app
+wget https://raw.githubusercontent.com/sinny777/hbuddy-gateway/master/gateway/app/resources/init.d/.asoundrc
+mv .asoundrc ~/.asoundrc
+
+docker run --rm -d -p 9000:9000 --name gateway-app -v ~/:/root -v /tmp:/tmp --privileged -it hukam/gateway-app
 
 sleep 3 &
 
